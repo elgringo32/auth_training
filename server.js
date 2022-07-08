67e7fec4ec4b9e3615340baa2c853fd2240ae580
Brian Knopke <BrianK@EDIT-2.local>
2022-07-08 13:37:48 -0700

    added register user schema, route, and request handler

--- server.js
+++ server.js
@@ -6,5 +6,14 @@ const connectDB = require('./db')
 //Connect DB via db.js
 connectDB()
 
+app.use(express.json())
+app.use('/api/Auth', require('./Auth/Route'))
+
 //setup server on port
 const server = app.listen(PORT, () => console.log(`server connected to port ${PORT}`))
+
+//log server connection error and close server
+process.on('unhandledRejection', err => {
+    console.log(`An error occurred: ${err.message}`)
+    server.close(() => process.exit(1))
+})
